1b06d22c16fb82c8db06d3591e8bce277be93199
chendy <dengyue.chen@ebaotech.com>
2017-07-20 16:01:24 +0800

    fix shell

--- get_docker_kue.sh
+++ get_docker_kue.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 namespace=docker-all.repo.ebaotech.com/libac
 images=(kube-proxy-amd64:v1.7.1 kube-scheduler-amd64:v1.7.1 kube-controller-manager-amd64:v1.7.1 kube-apiserver-amd64:v1.7.1 etcd-amd64:3.0.17 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.6.1 k8s-dns-sidecar-amd64:1.14.4 k8s-dns-kube-dns-amd64:1.14.4 k8s-dns-dnsmasq-nanny-amd64:1.14.4)
 
