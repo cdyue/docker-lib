d8e39e45a0b28b1ffc3a5c08cdbe525583dc53b4
chendy <dengyue.chen@ebaotech.com>
2018-01-17 19:14:18 +0800

    no message

--- get_docker_kue.sh
+++ get_docker_kue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 namespace=docker-all.repo.ebaotech.com/libac
-images=(kube-proxy-amd64:v1.7.1 kube-scheduler-amd64:v1.7.1 kube-controller-manager-amd64:v1.7.1 kube-apiserver-amd64:v1.7.1 etcd-amd64:3.0.17 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.6.1 k8s-dns-sidecar-amd64:1.14.4 k8s-dns-kube-dns-amd64:1.14.4 k8s-dns-dnsmasq-nanny-amd64:1.14.4)
+images=(kube-proxy-amd64:v1.7.12 kube-scheduler-amd64:v1.7.12 kube-controller-manager-amd64:v1.7.12 kube-apiserver-amd64:v1.7.12 etcd-amd64:3.0.17 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.7.1 k8s-dns-sidecar-amd64:1.14.5 k8s-dns-kube-dns-amd64:1.14.5 k8s-dns-dnsmasq-nanny-amd64:1.14.5)
 
 for imageName in ${images[@]} ; do
   docker pull $namespace/$imageName
